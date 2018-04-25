e76c3eab235032758e92a1c87163dd43cc547f2b
MarioJuniorPro <mario.junior.pro@gmail.com>
2018-04-24 23:59:24 -0300

    fix: photo link

--- js/app.js
+++ js/app.js
@@ -7,12 +7,12 @@ function createSkillElement(name, icon, level){
   level = level || 0;
   
   var levelString = '';
-  for(var i = 0; i < level; i++){
-    levelString += '<i class="fa fa-coffee"></i>'
-  }
+  // for(var i = 0; i < level; i++){
+  //   levelString += '<i class="fa fa-coffee"></i>'
+  // }
   
   var templateString = `
-  <div class="icon"><i class="fa ${icon || ''}"></i></div>
+  <div class="icon"><i class="${icon || ''}"></i></div>
   <div class="legend">
   ${name || ''}
   </div>
@@ -23,17 +23,20 @@ function createSkillElement(name, icon, level){
 }
 
 var skills = [
-  {icon: 'icon-html5', name: 'HTML5', level: 4},
-  {icon: 'icon-css3', name: 'CSS3', level: 3},
-  {icon: 'icon-sass', name: 'SASS', level: 2},
-  {icon: 'icon-javascript', name: 'JS', level: 4},
-  {icon: 'icon-angular', name: 'Angular 1.x', level: 4},
-  {icon: 'icon-nodejs', name: 'NodeJS', level: 3},
-  {icon: 'icon-java-bold', name: 'Java', level: 4},
-  {icon: 'icon-spring', name: 'Spring', level: 4},
-  {icon: 'icon-php-alt', name: 'PHP', level: 3},
-  {icon: 'icon-laravel', name: 'Laravel', level: 3},
-  {icon: 'icon-codeigniter', name: 'Code Igniter', level: 2},
+  {icon: 'fa icon-html5', name: 'HTML5', level: 4},
+  {icon: 'fa icon-css3', name: 'CSS3', level: 3},
+  {icon: 'fa icon-sass', name: 'SASS', level: 2},
+  {icon: 'fa icon-javascript', name: 'JS', level: 4},
+  {icon: 'fa icon-angular', name: 'Angular 1.x', level: 4},
+  {icon: 'fab fa-react', name: 'React', level: 4},
+  {icon: 'fab fa-react', name: 'React Native', level: 2},
+  {icon: 'fab fa-vuejs', name: 'Vuejs', level: 2},
+  {icon: 'fa icon-nodejs', name: 'NodeJS', level: 3},
+  {icon: 'fa icon-java-bold', name: 'Java', level: 4},
+  {icon: 'fa icon-spring', name: 'Spring', level: 4},
+  {icon: 'fa icon-php-alt', name: 'PHP', level: 3},
+  {icon: 'fa icon-laravel', name: 'Laravel', level: 3},
+  // {icon: 'fa icon-codeigniter', name: 'Code Igniter', level: 2},
 ]
 
 
